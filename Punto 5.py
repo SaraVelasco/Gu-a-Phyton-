96ccc4150373816c13f75c58a0b0aca709a9bd39
SaraVelasco <saracaranton@gmail.com>
2024-07-10 16:43:31 -0500

    Add files via upload

---
+++ Punto 5.py
@@ -0,0 +1,6 @@
+#Realice un programa que pregunte la edad del usuario en la consola y después de que el usuario lo introduzca
+#muestre por pantalla la cadena anterior junto con el nombre del usuario
+
+NaMe = input("Escribe tu nombre: ")
+print("¡Hola "+NaMe+"!")
+
