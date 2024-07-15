801788d78d61a100afbe8ab7ba2a29781ce8ad9f
SaraVelasco <saracaranton@gmail.com>
2024-07-15 16:55:18 -0500

    Add files via upload

---
+++ Punto 15.py
@@ -0,0 +1,14 @@
+
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si
+#es par o impar.   
+
+Numberr = int(input("Ingrese un número entero: "))
+
+if Numberr % 2 == 0:
+
+    print("es par")
+
+else:
+
+    print("es impar")    
+
