b9e4a7d412fcd47ea310ff6887885c5775e8d62c
SaraVelasco <saracaranton@gmail.com>
2024-07-15 16:06:51 -0500

    Add files via upload

---
+++ Punto 12.py
@@ -0,0 +1,12 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es
+#mayor de edad o no.
+
+NaMe = int(input("¿Cuál es tu edad? "))
+
+if NaMe < 18:
+
+    print("Eres menor de edad XD")
+
+else:
+
+    print("Eres mayor de edad >:D")    
