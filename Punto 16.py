6424e62ac8473d238709399654288113dbed0071
SaraVelasco <saracaranton@gmail.com>
2024-07-15 17:00:22 -0500

    Add files via upload

---
+++ Punto 16.py
@@ -0,0 +1,16 @@
+
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla
+#10 veces.
+
+Word = input("Ingresa una palabra: ")
+
+print(Word)
+print(Word)
+print(Word)
+print(Word)
+print(Word)
+print(Word)
+print(Word)
+print(Word)
+print(Word)
+print(Word)
