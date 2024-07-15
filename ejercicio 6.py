779eab6f26f9eed857e4f218d59bbd7340623acb
SaraVelasco <saracaranton@gmail.com>
2024-07-15 13:04:34 -0500

    Add files via upload

---
+++ ejercicio 6.py
@@ -0,0 +1,12 @@
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación:
+# (3+2/2*5)2
+
+suma = (3+2)
+
+multiplicación = (2*5)
+
+división = (suma/multiplicación)
+
+resultado = (división*división)
+
+print(resultado)
